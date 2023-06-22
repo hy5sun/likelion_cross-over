522ae42ed97f3735613864b9b6948d910fe16440
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-06-22 13:29:52 +0900

    LoginDto에서 글자 제한 제거

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,9 @@
-import { Matches } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class LoginDto {
-  @Matches(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{5,9}$/)
+  @IsString()
   id: string;
 
-  @Matches(
-    /^(?=.*[a-zA-Z])(?=.*[~!@#$%^&*_\-+=`|\(){}[\]:;"'<>,.?/])(?=.*[0-9]).{8,13}$/,
-  )
+  @IsString()
   password: string;
 }
