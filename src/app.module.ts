f12c609a59bde586f174161df91531f80d011079
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-06-24 11:57:22 +0900

    synchronize -> false

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { CommonModule } from './common/common.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      synchronize: true,
+      synchronize: false,
       autoLoadEntities: true,
       charset: 'utf8mb4',
       logging: true,
